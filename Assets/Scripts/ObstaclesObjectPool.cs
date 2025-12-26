2194de8872cce5c7060f1d41b1898fa1898f81e7
SaiGaneshr <63908391+SaiGaneshRachamadugu@users.noreply.github.com>
2025-12-26 12:48:57 +0530

    logs while fetching back from pool

--- Assets/Scripts/ObstaclesObjectPool.cs
+++ Assets/Scripts/ObstaclesObjectPool.cs
@@ -43,6 +43,7 @@ public class ObstaclesObjectPool : MonoBehaviour
         {
             Debug.LogWarning($"Pool empty for {prefab.name}");
             return null;
+            Debug.log("@@back from pool");
         }
 
         GameObject obj = pool.Dequeue();
